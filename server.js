97c9d0b397c6b617cb2a91ec0c27835a34065ca2
imxitiz <69164340+imxitiz@users.noreply.github.com>
2024-11-14 16:55:34 +0545

    prod ready
    --
    Signed-off-by: Kshitiz Sharma (imxitiz)   <69164340+imxitiz@users.noreply.github.com>

--- server.js
+++ server.js
@@ -84,6 +84,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
